9694313fef8d6672a61d3417ff85ce1d8ba50cf3
David Dyess <david.dyess@gmail.com>
2010-12-25 02:07:26 -0600

    Initial commit

---
+++ info.php
@@ -0,0 +1,10 @@
+<?php
+$data = array('name' => 'Creole',
+			'directory' => 'creole',
+			'version' => '1.1.0',
+			'date' => 'November 5, 2009',
+			'description' => "Creole is a database abstraction layer for PHP5. It abstracts PHP's native db-specific API to create more portable code while also providing developers with a clean fully object-oriented interface based loosely on the API for Java's JDBC.",
+			'class' => 'System',
+			'category' => 'Tools',
+			);
+?>
